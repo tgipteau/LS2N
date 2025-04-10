- fixed gamma sign - dual method for random init state (fiso3 and fiso4)

Fiso3/fiso3.py
@@ -143,6 +143,50 @@ dtau = dt / 2
 kx = delta * dtau / dx ** 2
 ky = delta * dtau / dy ** 2
 
+init_method = "naive"
+
+
+def randomize_starter(uplus, wplus, umoins, wmoins):
+    
+    if init_method == "naive":
+        # méthode naive : état instable +- 0.5 partout, sur chaque point
+        u_0 = np.zeros(J * J)
+        w_0 = np.zeros(J * J)
+        for i in range(J):
+            for j in range(J):
+                u_0[i * J + j] = umoins + (-1 + 2 * random.random()) * 0.5
+                w_0[i * J + j] = wmoins + (-1 + 2 * random.random()) * 0.5
+    
+        return u_0, w_0
+    
+    elif init_method == "bumps":
+        # méthode "à bosses"
+        max_bosses = 50
+        max_height = uplus
+        max_d = 20
+        
+        nb_bosses = random.randint(1, max_bosses)
+        u_0 = np.zeros(J * J)
+        w_0 = np.zeros(J * J)
+        
+        for _ in range(nb_bosses):
+            
+            h = random.random() * max_height
+            d = random.randint(1, max_d)
+            cx = random.randint(0, J)
+            cy = random.randint(0, J)
+            
+            print(f"h = {h}, d = {d}, cx = {cx}, cy = {cy}")
+            
+            bosse_func = lambda i, j: h * d / ((cx - i) ** 2 + (cy - j) ** 2 + d)
+            
+            for i in range(J):
+                for j in range(J):
+                    u_0[i * J + j] += bosse_func(i, j)
+                    w_0[i * J + j] += bosse_func(i, j)
+        
+        return u_0, w_0
+    
 
 # Termes de réaction du modèle
 def reaction(X, t):
@@ -201,12 +245,7 @@ def run_simulation(save_folder="Simulations/default"):
     y = np.linspace(0.0, L, J)
     X = np.linspace(0.0, L * L, J * J)
     
-    u0 = np.zeros(J * J)
-    w0 = np.zeros(J * J)
-    for i in range(J):
-        for j in range(J):
-            u0[i * J + j] = umoins + (-1 + 2 * random.random()) * 0.5
-            w0[i * J + j] = wmoins + (-1 + 2 * random.random()) * 0.5
+    u0, w0 = randomize_starter(uplus, wplus, umoins, wmoins)
     
     # Matrice du schéma
     diag = np.ones(J)
@@ -722,7 +761,7 @@ if __name__ == "__main__":
             video_writer.write(frame)
             
             # Sauvegarde en JPEG (plan B)
-            frame_filename = os.path.join(frames_folder, f"frame_{t}.jpg")
+            frame_filename = os.path.join(frames_folder, f"frame_{t:02d}.jpg")
             cv2.imwrite(frame_filename, frame)
         ##########################################
         ## Gestion des évenements

Fiso4/fiso4.py
@@ -160,6 +160,48 @@ ky_b = delta_b * dtau / dy ** 2
 kx_m = delta_m * dtau / dx ** 2
 ky_m = delta_m * dtau / dy ** 2
 
+init_method = "naive"
+
+
+def randomize_starter(uplus, wplus, umoins, wmoins):
+    if init_method == "naive":
+        # méthode naive : état instable +- 0.5 partout, sur chaque point
+        u_0 = np.zeros(J * J)
+        w_0 = np.zeros(J * J)
+        for i in range(J):
+            for j in range(J):
+                u_0[i * J + j] = umoins + (-1 + 2 * random.random()) * 0.5
+                w_0[i * J + j] = wmoins + (-1 + 2 * random.random()) * 0.5
+        
+        return u_0, w_0
+    
+    elif init_method == "bumps":
+        # méthode "à bosses"
+        max_bosses = 50
+        max_height = uplus
+        max_d = 20
+        
+        nb_bosses = random.randint(1, max_bosses)
+        u_0 = np.zeros(J * J)
+        w_0 = np.zeros(J * J)
+        
+        for _ in range(nb_bosses):
+            
+            h = random.random() * max_height
+            d = random.randint(1, max_d)
+            cx = random.randint(0, J)
+            cy = random.randint(0, J)
+            
+            print(f"h = {h}, d = {d}, cx = {cx}, cy = {cy}")
+            
+            bosse_func = lambda i, j: h * d / ((cx - i) ** 2 + (cy - j) ** 2 + d)
+            
+            for i in range(J):
+                for j in range(J):
+                    u_0[i * J + j] += bosse_func(i, j)
+                    w_0[i * J + j] += bosse_func(i, j)
+        
+        return u_0, w_0
 
 # Termes de réaction du modèle
 
@@ -183,8 +225,6 @@ def reaction(X, t, i):
     dwm = -beta_m * wm + alpha_m * um
 
     return [dub, dum, dwb, dwm]
-
-
 
 
 
@@ -231,20 +271,10 @@ def run_simulation(save_folder="Simulations/default"):
     y = np.linspace(0.0, L, J)
     X = np.linspace(0.0, L * L, J * J)
     
-    u_b0 = np.zeros(J * J)
-    w_b0 = np.zeros(J * J)
-    for i in range(J):
-        for j in range(J):
-            u_b0[i * J + j] = umoins + (-1 + 2 * random.random()) * 0.5
-            w_b0[i * J + j] = wmoins + (-1 + 2 * random.random()) * 0.5
+    u_b0, w_b0 = randomize_starter(uplus, wplus, umoins, wmoins)
     
-    u_m0 = np.zeros(J * J)
-    w_m0 = np.zeros(J * J)
-    for i in range(J):
-        for j in range(J):
-            u_m0[i * J + j] = umoins + (-1 + 2 * random.random()) * 0.5
-            w_m0[i * J + j] = wmoins + (-1 + 2 * random.random()) * 0.5
-    
+    u_m0, w_m0 = randomize_starter(uplus, wplus, umoins, wmoins)
+ 
     # Matrices du schéma : boreale
     diag = np.ones(J)
     diagsup = np.ones(J - 1)
@@ -826,7 +856,7 @@ if __name__ == "__main__":
             video_writer.write(frame)
             
             # Sauvegarde en JPEG (plan B)
-            frame_filename = os.path.join(frames_folder, f"frame_{t}.jpg")
+            frame_filename = os.path.join(frames_folder, f"frame_{t:02d}.jpg")
             cv2.imwrite(frame_filename, frame)
         ##########################################
         ## Gestion des évenements