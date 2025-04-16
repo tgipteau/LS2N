61b875661fdeadfd76e2617185ef2b827bb15151
CarlMankey <carlmankey@gmail.com>
2025-04-16 14:45:03 +0200

    hmm

--- HMM/HMMmain.py
+++ HMM/HMMmain.py
@@ -1,12 +1,15 @@
+import os
+
 from computes import norm_ueq, get_dfs, plots
 from simu import run_simulation, T_linspace
 from tqdm import tqdm
     
 if __name__ == "__main__":
     
-    nb_simulations = 100
+    nb_simulations = 25
     range_sim = range(0, nb_simulations)
     Ns = []
+    os.makedirs("Simulations", exist_ok=True)
     
     for i in tqdm(range_sim):
         
@@ -15,7 +18,6 @@ if __name__ == "__main__":
         df, df_feux = get_dfs(sim_name=sim_name)
         N, val_span = norm_ueq(U=df, T=T_linspace, uplus=uplus, wplus=wplus)
         Ns.append(N)
-        print(N)
         
     plots(Ns, T_linspace, val_span, uplus, wplus)
     
